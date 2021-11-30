ac2849e4161bf8b6ceac0c3a6bbf04f3a3b307dd
yarpiin <yarpiin@gmail.com>
2021-11-30 20:09:56 +0000

    umi: drop custom init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,13 +16,13 @@ TARGET_OTA_ASSERT_DEVICE := umi
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_umi
-TARGET_RECOVERY_DEVICE_MODULES := libinit_umi
+# Fingerprint
+BUILD_FINGERPRINT := Xiaomi/umi/umi:11/RKQ1.200826.002/V12.5.10.0.RJBCNXM:user/release-keys
 
 # Kernel
 TARGET_KERNEL_CONFIG := umi_user_defconfig
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 
