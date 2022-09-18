43aacf97d88f32a9d7c430e586ca3f6d8e19dc11
yarpiin <yarpiin@gmail.com>
2022-09-18 21:12:16 +0100

    umi: drop stock fingerprint

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@ TARGET_OTA_ASSERT_DEVICE := umi
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Fingerprint
-BUILD_FINGERPRINT := Xiaomi/umi/umi:12/SKQ1.211006.001/V13.0.7.0.SJBCNXM:user/release-keys
-
 # Kernel
 TARGET_KERNEL_CONFIG := umi_user_defconfig
 
